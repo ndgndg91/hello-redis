15f5763336f131aa372b0f49d21aacf1afe934c7
ndgndg91 <ndgndg91@gmail.com>
2022-10-08 21:08:19 +0900

    <feat> cache aside pattern with ttl

--- spring-reactive-data-redis-playground/src/main/java/com/ndgndg91/springreactivedataredisplayground/city/service/CityService.java
+++ spring-reactive-data-redis-playground/src/main/java/com/ndgndg91/springreactivedataredisplayground/city/service/CityService.java
@@ -2,29 +2,30 @@ package com.ndgndg91.springreactivedataredisplayground.city.service;
 
 import com.ndgndg91.springreactivedataredisplayground.city.City;
 import com.ndgndg91.springreactivedataredisplayground.city.repository.CityRepository;
-import org.redisson.api.RMapReactive;
+import org.redisson.api.RMapCacheReactive;
 import org.redisson.api.RedissonReactiveClient;
 import org.redisson.codec.TypedJsonJacksonCodec;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 import java.math.BigInteger;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class CityService {
 
     private final CityRepository repository;
 
-    private final RMapReactive<BigInteger, City> map;
+    private final RMapCacheReactive<BigInteger, City> map;
 
     public CityService(CityRepository repository, RedissonReactiveClient client) {
         this.repository = repository;
-        this.map = client.getMap("city", new TypedJsonJacksonCodec(BigInteger.class, City.class));
+        this.map = client.getMapCache("city", new TypedJsonJacksonCodec(BigInteger.class, City.class));
     }
 
     public Mono<City> findCity(final BigInteger id) {
         return map.get(id).switchIfEmpty(
-                repository.findCityById(id).flatMap(c -> map.fastPut(id, c).thenReturn(c))
+                repository.findCityById(id).flatMap(c -> map.fastPut(id, c, 10, TimeUnit.SECONDS).thenReturn(c))
         );
     }
 }
